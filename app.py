4d15545a50501038d8cce036792f00cf89678c6f
rafael.targino <rafaeltargino@gmail.com>
2024-10-24 23:26:54 -0300

    Add streamlit app with data loading functions and sidebar filters

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+#criar funções de carregamento de dados
+#@st.cache_data   # decorator para atribuir uma funcionalidade extra para uma função 
+#def carregar_cotacoes(empresas):
+
+# criar a interface do streamlit
+st.write("""
+# brewlytics
+
+""") # pode formatar com markdown
+
+# sidebar
+st.sidebar.header("Filtros")  
