49d1c45824b35eff318f9ad9ee50be383f8e3832
rafael.targino <rafaeltargino@gmail.com>
2024-10-26 20:41:17 -0300

    Include two columns

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import streamlit as st
 from sidebar import load_sidebar
+from beeranalysis_one import load_beer_analysis_one
+from beeranalysis_all import load_beer_analysis_all
 
 #criar funções de carregamento de dados
 #@st.cache_data   # decorator para atribuir uma funcionalidade extra para uma função 
 #def carregar_cotacoes(empresas):
 
-# criar a interface do streamlit
-st.write("""
-# 🍻 brewlytics
+# Ajustar as margens para deixar a tela mais larga
+st.set_page_config(page_title="brewlytics", layout="wide")
 
-""") # pode formatar com markdown
+# Título inicial
+st.write("# 🍻 brewlytics")
 
-# sidebar 
-# --------------------------------------------------------------------------------
-
+# carregando a sidebar com os estilos e descritores
 load_sidebar()
 
+# Separando a tela principal em duas coluanas: análise do grupo e análise individual
+col_all, col_one = st.columns(2)
 
+# Análise do grupo
+load_beer_analysis_all(col_all)
 
-# --------------------------------------------------------------------------------
-
-
+# Análise individual
+load_beer_analysis_one(col_one)
---
+++ beeranalysis_one.py
@@ -0,0 +1,23 @@
+import json
+import streamlit as st
+
+#criar funções de carregamento de dados
+@st.cache_data   # decorator para atribuir uma funcionalidade extra para uma função
+def load_bjcp_descriptors():
+    # Carregar o arquivo JSON
+    with open('bjcp_keywords.json', 'r', encoding='utf-8') as arquivo:
+        beerstyle_dict = json.load(arquivo)
+    #print(beerstyle_dict)
+    return beerstyle_dict
+
+def get_beer_style_descriptors(beer_style):
+    beerstyle_dict = load_bjcp_descriptors()
+    return beerstyle_dict.get(beer_style, {})
+
+
+def load_beer_analysis_one(coluna):
+    
+    with coluna:
+        st.header("Análise Individual")
+        st.write("Este é o conteúdo da coluna 1.")
+        st.button("Botão 2")
